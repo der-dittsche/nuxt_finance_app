5307f63473d85b44d7cc2f166a3bcc85c73c1961
Sascha Dietrich <s.dietrich@computy.de>
2024-04-05 14:06:48 +0200

    initial config and initial layout added

---
+++ components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<header class="flex justify-between items-center mt-10">
+  <NuxtLink to="/" class="text-xl font-bold">
+    Finance Tracker
+  </NuxtLink>
+  <div>
+    <UAvatar src="https://avatars.githubusercontent.com/u/109524890?v=4" alt="Avatar" />
+  </div>
+</header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="container mx-auto max-w-4xl">
+  <AppHeader />
+  <main class="my-10">
+    <slot />
+  </main>
+</div>
+</template>
+
+<style scoped>
+body {
+  font-family: monospace;
+}
+
+body {
+  @apply dark:bg-gray-900 bg-white
+}
+</style>
