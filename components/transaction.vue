566a8c018ad42133d2ad54af83a212bcb3775bcf
Sascha Dietrich <sascha_dietrich@outlook.com>
2024-04-07 17:17:50 +0200

    feat: finish layout for summary

---
+++ components/transaction.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">import {useCurrency} from "~/composable/useCurrency";
+
+const {currency} = useCurrency(3000, 'EUR')
+
+const items = [
+[{
+  label: 'Edit',
+  icon: 'i-heroicons-pencil-square-20-solid',
+  click: () => console.log('edit')
+},
+{
+  label: 'Delete',
+      icon:'i-heroicons-trash-20-solid',
+      click:  () => console.log('delete')
+}]
+]
+</script>
+
+<template>
+  <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-1">
+        <UIcon name="i-heroicons-arrow-up-right" class="text-green-600"/>
+        <div>Salary</div>
+      </div>
+      <div>
+        <UBadge color="white">Category</UBadge>
+      </div>
+    </div>
+    <div class="flex items-center justify-end space-x-2">
+      <div>{{ currency }}</div>
+      <div>
+        <UDropdown :items="items" :propper="{placement: 'bottom-start'}">
+          <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
+        </UDropdown>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {transactionViewOptions} from "~/constants";
+import Transaction from "~/components/transaction.vue";
 const selectedView = ref(transactionViewOptions[1])
 </script>
 
@@ -12,11 +13,19 @@ const selectedView = ref(transactionViewOptions[1])
       <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
     </div>
   </section>
+
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
     <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
     <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="false" />
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false"/>
     <Trend color="red" title="Savings" :amount="4000" :last-amount="8000" :loading="true" />
+  </section>
+
+  <section>
+    <Transaction />
+    <Transaction />
+    <Transaction />
+    <Transaction />
   </section>
 </template>
 
